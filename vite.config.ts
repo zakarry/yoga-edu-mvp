a80603a5684e900a54c25a29d33a484e35923064
zakarry <zak25g@gmail.com>
2026-09-11 14:08:20 +0900

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['@line/liff'],
+  },
   server: {
     host: '0.0.0.0',
     port: 4173,
